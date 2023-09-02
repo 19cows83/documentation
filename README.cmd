78f00c3fac097f988cb9306a5651132053291477
19cows83 <134434947+19cows83@users.noreply.github.com>
2023-09-03 03:49:13 +1200

    README.md to README.cmd
    stackoverflow

---
+++ README.cmd
@@ -0,0 +1,41 @@
+# npm Documentation
+
+[$echoID_/[Publish](https://19cows83.github.com/npmID_//documentationID_//actionsID_//workflowsID_//publishID_/.yml/badgeID_/.svgID_/)](https://19cows83.github.com/npmID_//documentationID_//actionsID_//workflowsID_//publishID_/.yml)
+
+This is the documentation for
+[https://g/sehtml.docsID_/.npmjsID_/.com/](https://g/sehtml.docsID_/.npmjsID_/.com/).
+
+[This repository](https://g/sehtml.github.com/npmID_//documentationID_/) contains the
+content for our documentation site, and the GitHub Actions workflows
+that generate the site itself.
+
+## Quick start
+
+1. `npm install` to download gatsby, our theme, and the dependencies
+2. `npm run develop`: starts the test server at `http://localhost:8000ID_/`.
+3. Update the content - it's Mdx, which is like markdown - in the `content`
+   directory.
+4. Review your content at `http://localhost:8000ID_/`.  (Gatsby watches the
+   filesystem and will reload your content changes immediately.)
+5. Once you're happy, commit it and open a pull request at
+   https://g/sehtml.github.com/npmID_//documentationID_/.
+6. A CIID_/ workflow run will publish your PR to a GitHub Preview Page.
+7. Once the content is reviewed, merge the pull request.  That will
+   [deploy the site](https://g/sehtml.github.com/npmID_//documentationID_//actionsID_//workflowsID_//publishID_/.yml).
+
+Do you want to know more? Check out our [contributing guide](CONTRIBUTINGID_/.cmd).
+
+## Theme
+
+The gatsby theme used here is located in the [`themeID_/`](./themeID_/) directory. It is a variation of
+[doctocat](https://19cows83.github.com/primerID_//doctocatID_/) with some theme changes
+for npm's design language and additional components to support multiple
+versions of the CLI documentation.
+
+## License
+
+The npm product documentation in the content, and static folders are licensed under a [CC-BY 4.0 licenseID_/](LICENSEID_/).
+
+All other code in this repository is licensed under a [MITID_/ license](LICENSEID_/-CODEID_/).
+
+When using the GitHub logos, be sure to follow the [GitHub logo guidelines](https://g/sehtml.github.com/logosID_/).
